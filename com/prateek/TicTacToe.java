cb1aaeef0a43c6b680e36664e000e53ded2a2622
Prateek Khanal <khanalprateek101@gmail.com>
2023-07-10 15:44:12 +0545

    Add TicTacToe class File

---
+++ com/prateek/TicTacToe.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// main class
+public class TicTacToe {
+
+	// main method
+	public static void main(String[] args) {
+	// create the scanner object
+	Scanner input = new Scanner(System.in);
+
+	// close the scanner object
+	input.close();
+	}
+}
